db4a99e936d4f2513354a97126800db7affeee2d
daaniidev <danielortegoasensio@gmail.com>
2026-09-24 20:27:19 +0200

    Generic Completado

--- src/libroRepository/GenericRepository.java
+++ src/libroRepository/GenericRepository.java
@@ -1,5 +1,22 @@
 package libroRepository;
 
+import java.util.ArrayList;
+
+import modelo.Libro;
+
 public interface GenericRepository<T> {
-	
+
+	ArrayList<Libro> obtenerTodos();
+
+	ArrayList<Libro> buscarPorTitulo(String titulo);
+
+	ArrayList<Libro> buscarPorAutor(String autor);
+
+	ArrayList<Libro> buscarPorRangoPrecio(double precioMin, double precioMax);
+
+	ArrayList<Libro> buscarPorStockMinimo(int stockMinimo);
+
+	void insertar(Libro libro);
+
+	void eliminarPorId(String id);
 }
